69b4cd3674a44ad2154c8cb90ff626e648dde574
hishamabdalla <hishvmabdalla@gmail.com>
2025-07-19 02:36:37 +0300

    Refactor and clean up Program.cs for better readability

---
+++ Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Repositories;
+using Service.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+            services.AddDbContextServices(configuration);
+            services.AddPersistenceRepositories();
+        }
+
+        private static void AddDbContextServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PharmaProjectContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("RemoteConnection")));
+        }
+
+        private static void AddPersistenceRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IStockRepository, StockRepository>();
+            services.AddScoped<IMedicineBatchRepository, MedicineBatchRepository>();
+            services.AddScoped<IMedicineRepository, MedicineRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+
+        }
+    }
+}
--- PharmaTrack/Program.cs
+++ PharmaTrack/Program.cs
@@ -11,58 +11,22 @@ using Services.AutoMapper.CategoryAtoMapper;
 using Services.Services;
 using Services.AutoMapper.MedicineAtoMapper;
 using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using PharmaTrack.Helper;
+using System.Threading.Tasks;
 
 namespace PharmaTrack
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-           // builder.Services.AddOpenApi();
-            builder.Services.AddDbContext<PharmaProjectContext>(options =>
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
-
-            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            builder.Services.AddScoped<IStockRepository, StockRepository>();
-            builder.Services.AddScoped<IMedicineBatchRepository, MedicineBatchRepository>();
-            builder.Services.AddScoped<IStockService, StockService>();
-            
-
-            builder.Services.AddAutoMapper(map => map.AddProfile(new CategoryMappingProfile()));
-            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-            builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-            builder.Services.AddAutoMapper(map => map.AddProfile(new MedicineMappingProfile()));
-            builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
-            builder.Services.AddScoped<IMedicineSercive,MedicineSercive>();
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
+            builder.Services.AddDependency(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            await app.ConfigureMiddlewareServices();
 
             app.Run();
         }
